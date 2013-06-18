1bd99f749bd9072543b7e1c7fe4a1cb6d07b7a77
Michael Schilling <maschill92@gmail.com>
2013-06-18 08:42:57 -0500

    UI Commit #1
    Old UI changes

--- src/io/github/maschill92/diceroller/MainActivity.java
+++ src/io/github/maschill92/diceroller/MainActivity.java
@@ -1,22 +1,29 @@
 package io.github.maschill92.diceroller;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Spinner spin = (Spinner) findViewById(R.id.spinner_rollCount);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.intArray_rollCount, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
 }
